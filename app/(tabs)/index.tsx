e20554c1803220f539088d104164c062d27851a9
kawajiharuka <haruka.kawaji@blueish.co.jp>
2024-11-30 09:31:41 +0900

    feat：タブ遷移ベース

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,22 @@
-import { StyleSheet, Text } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function HomeScreen() {
-  return <Text>テスト</Text>;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>ホーム</Text>
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
